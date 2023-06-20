5dd1fb9b7f2646f490da3bc6b10a8e7fd3433103
ohmono <juan.cardona32@udea.edu.co>
2023-06-20 18:22:13 -0500

    .

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,14 +1,12 @@
 from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 from functions import auth
-from schemas.auth import *
+from schemas.auth import (Token, Login)
 
 
 router = APIRouter(tags=["Authentication"], prefix="/auth")
 
 
 @router.post("/login", response_model=Token)
-def login(request: Login):#:schemas.Login
+def login(request: OAuth2PasswordRequestForm = Depends()):  # :Login
     return auth.login(request)
-
-
---
+++ notas.md
@@ -0,0 +1,11 @@
+
+```{
+  "grant_type": null,
+  "username": "q@q.q",
+  "password": "string",
+  "scopes": [],
+  "client_id": null,
+  "client_secret": null
+}```
+
+estructura de petición a login en formato oauth2
