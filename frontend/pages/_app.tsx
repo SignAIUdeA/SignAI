261e041b6e9c331304380c1ed855637d6b14cea7
Jose Waldo <josferw@gmail.com>
2023-08-30 10:17:24 -0500

    style: add font family in all project

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Jost } from "next/font/google";
+
+const jost = Jost({ subsets: ["latin"], weight: ["500", "600", "700"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={jost.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
