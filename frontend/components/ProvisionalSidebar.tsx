2435fc226d96614e5d54956bacd142446bcf463d
santiago1023 <santiago.bedoyad@udea.edu.co>
2023-08-30 16:57:02 -0500

    Avancé en la vista del ProfesionalDashboard

--- frontend/components/ProvisionalSidebar.tsx
+++ frontend/components/ProvisionalSidebar.tsx
@@ -1,14 +1,35 @@
+
 import React from 'react'
+import { MdLeaderboard, MdManageAccounts, MdMarkAsUnread,
+  MdArticle, MdSettings, MdMail,
+   MdFileUpload } from "react-icons/md";
 
 const ProvisionalSidebar = () => {
   return (
-    <aside className='hidden w-64 md:flex flex-col gap-12 pt-12 pl-12'>
-        <div>Logo</div>
+    <aside className='hidden w-64 md:flex flex-col gap-12 pt-12 pl-12 debug'>
+        <div className='debug flex gap-4 items-center'>
+          <button className='bg-white debug'>
+            <MdArticle className='h-8 w-8'/>
+          </button>
+          <span>SignAI</span>
+        </div>
         <div className='flex flex-col gap-6'>
-            <div>Bandeja de Entrada</div>
-            <div>Subir Señas</div>
-            <div>Análisis de Datos</div>
-            <div>Configurar Perfil</div>
+            <button className='debug flex bg-white rounded-lg'>
+              <MdMarkAsUnread className='h-8 w-8'/>
+              <span>Bandeja de Entrada</span>
+            </button>
+            <button className='flex '>
+              <MdFileUpload className='h-8 w-8'/>
+              <span></span>Subir Señas
+            </button>
+            <button className='flex '> 
+              <MdLeaderboard className='h-8 w-8'/>
+              <span></span>Análisis de Datos
+            </button>
+            <button className='flex '>
+              <MdManageAccounts className='h-8 w-8'/>
+              <span></span>Configurar Perfil
+            </button>
         </div>
     </aside>
   )
--- frontend/pages/ProfesionalDashboard.tsx
+++ frontend/pages/ProfesionalDashboard.tsx
@@ -1,5 +1,7 @@
-import Layout from '@/layouts/Layout'
 import React from 'react'
+import Layout from '@/layouts/Layout'
+import Image from 'next/image'
+import { MdDateRange, MdLabelOutline, MdOutlinePermIdentity } from 'react-icons/md'
 
 const ProfesionalDashboard = () => {
   return (
@@ -17,7 +19,7 @@ const ProfesionalDashboard = () => {
                     <button>der</button>
                 </div>
             </div>
-            <DesktopTable/>
+            <DesktopCards/>
             <MobileCards/>
         </div>
       </Layout>
@@ -25,37 +27,65 @@ const ProfesionalDashboard = () => {
   )
 }
 
-const DesktopTable = () => {
+const Card = () => {
   return(
-    <div className='hidden md:flex h-full flex-col'>
-      <div className='h-full mt-6 debug'>tabla</div>
-      <div>paginacion</div>
+    <div className='flex flex-col debug w-44 h-[250px]'>
+      <Image  src="/sign.png" width={176} height={100} alt="sign"/>
+      <div className='flex flex-col gap-2 p-2 bg-[#E3F8ED] debug'>
+        <div className='flex debug'>
+          <MdOutlinePermIdentity className='h-5 w-5'/>
+          <span>Nombre</span>
+        </div>
+        <div className='flex debug'>
+          <MdDateRange className='h-5 w-5'/>
+          <span>Fecha</span>
+        </div>
+        <div className='flex debug'>
+          <MdLabelOutline className='h-5 w-5'/>
+          <span>Etiqueta</span>
+        </div>
+        <button className='w-30 h-5 flex justify-center items-center p-0 rounded-lg debug'>
+          <span>Etiquetar</span>
+        </button>
+      </div>
     </div>
   );
 };
+
+const DesktopCards = () => {
+  return (
+    <div className='hidden md:grid h-full mt-6 justify-items-center debug overflow-y-auto'>
+      <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
+};
+
 const MobileCards = () =>{
   return(
-    <div className='grid grid-cols-2 h-full md:hidden'>
-      <div className='flex flex-col debug'>
-        <div>Imagen</div>
-        <div className='flex flex-col'>
-          <span>Nombre</span>
-          <span>Fecha</span>
-          <span>Etiqueta</span>
-          <button>Etiquetar</button>
-        </div>
-      </div>
-      <div className='flex flex-col debug'>
-        <div>Imagen</div>
-        <div className='flex flex-col'>
-          <span>Nombre</span>
-          <span>Fecha</span>
-          <span>Etiqueta</span>
-          <button>Etiquetar</button>
-        </div>
+    <div className='md:hidden sm:grid h-full mt-6 justify-items-center debug'>
+      <div className='grid sm:grid-cols-2'>
+        <Card/>
+        <Card/>
+        <Card/>
       </div>
     </div>
   );
 };
+
 
 export default ProfesionalDashboard
