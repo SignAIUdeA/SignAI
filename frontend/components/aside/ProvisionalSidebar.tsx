540c1e35d802dfb7d09ac91e1c8f7d8aefc02f1e
santiago1023 <santiago.bedoyad@udea.edu.co>
2023-09-06 09:09:18 -0500

    Se cambió el sidebar

--- frontend/components/aside/ProvisionalSidebar.tsx
+++ frontend/components/aside/ProvisionalSidebar.tsx
@@ -1,9 +1,8 @@
 
 import React from 'react'
-import { MdLeaderboard, MdManageAccounts, MdMarkAsUnread,
-  MdArticle, MdSettings, MdMail,
-   MdFileUpload } from "react-icons/md";
-
+import { MdLeaderboard, MdManageAccounts, MdMarkAsUnread, MdArticle, MdFileUpload } from "react-icons/md";
+import { SidebarData } from './aside.services';
+import Link from 'next/link';
 const ProvisionalSidebar = () => {
   return (
     <aside className='hidden w-64 md:flex flex-col gap-12 pt-12 pl-12 debug'>
@@ -13,7 +12,21 @@ const ProvisionalSidebar = () => {
           </button>
           <span>SignAI</span>
         </div>
-        <div className='flex flex-col gap-6'>
+        
+        <ul className='flex flex-col gap-6'>
+          {SidebarData.map((item, index) => (
+            <li key={item.title} className=''>
+              <Link href={item.link} className='debug flex bg-white rounded-lg hover:bg-green-500 px-2 py-3 justify-start items-center active:bg-black'>
+                {item.icon}
+                <span className='text-sm'>
+                  {item.title}
+                </span>
+                </Link>
+            </li>
+          ))}
+        </ul>
+
+        {/* <div className='flex flex-col gap-6'>
             <button className='debug flex bg-white rounded-lg'>
               <MdMarkAsUnread className='h-8 w-8'/>
               <span>Bandeja de Entrada</span>
@@ -30,9 +43,15 @@ const ProvisionalSidebar = () => {
               <MdManageAccounts className='h-8 w-8'/>
               <span></span>Configurar Perfil
             </button>
-        </div>
+        </div> */}
     </aside>
   )
 }
 
+const sidebar = () => {
+  
+    <aside>
+      <ul></ul>
+    </aside>
+}
 export {ProvisionalSidebar};
