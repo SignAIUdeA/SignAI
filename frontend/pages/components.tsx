b4990f99cd2f4808cda0367b807a4eeb6b9ae11c
Jose Waldo <josferw@gmail.com>
2023-09-07 09:27:05 -0500

    Probando como funcionaria la navegación si el usuario se ha logueado

--- frontend/pages/components.tsx
+++ frontend/pages/components.tsx
@@ -1,12 +1,30 @@
 import Table from "@/components/table/Table";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 function Components() {
-  return (
-    <main className="p-10">
-      <h1 className="mb-[2rem]">Vista de los componentes</h1>
-      <Table />
-    </main>
-  );
+  const [isLogin, setIsLogin] = useState<boolean>(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const auth = sessionStorage.getItem("authInfo");
+    if (auth) {
+      setIsLogin(true);
+      return;
+    } else {
+      router.push("/newlogin");
+      return;
+    }
+  }, []);
+
+  if (isLogin) {
+    return (
+      <main className="p-10">
+        <h1 className="mb-[2rem]">Vista de los componentes</h1>
+        <Table />
+      </main>
+    );
+  }
 }
 
 export default Components;
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,23 +1,27 @@
-import Image from 'next/image';
-import escudoUdeA from '../public/escudoUdeA.png';
-import React from 'react'
-import Link from 'next/link';
+import Image from "next/image";
+import escudoUdeA from "../public/escudoUdeA.png";
+import React from "react";
+import Link from "next/link";
 import Logo from "@/components/Logo";
 
 const index = () => {
-  console.log(escudoUdeA)
+  console.log(escudoUdeA);
   return (
     <div className="welcome flex justify-center gap-3 m-4">
-		  <Logo/>     
+      <Logo />
       <h1>SignAI UdeA</h1>
-      <Link href="/login">
-        <button type='submit' className='primary-button'>Ingresar</button>
+      <Link href="/newlogin">
+        <button type="submit" className="primary-button">
+          Ingresar
+        </button>
       </Link>
       <Link href="/signup">
-        <button type='reset' className='secondary-button'>Registrarse</button>
+        <button type="reset" className="secondary-button">
+          Registrarse
+        </button>
       </Link>
-	  </div>
-  )
-}
+    </div>
+  );
+};
 
-export default index
+export default index;
