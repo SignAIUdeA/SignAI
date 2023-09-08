f0d4a8bb8defd15367825d310bdfae6bffc86952
Jose Waldo <josferw@gmail.com>
2023-09-08 08:48:29 -0500

    feat: add a component for the filters

---
+++ frontend/components/filter-section/FilterSection.tsx
@@ -0,0 +1,30 @@
+import { IconArrow, IconFilter } from "@/icons/Icons";
+import style from "./filter-section.module.css";
+
+const FilterSection = () => {
+  return (
+    <section className={style.FilterSection}>
+      <section className={style.BarSearch}>
+        <input
+          className={style.BarSearchInput}
+          type="text"
+          placeholder="Buscar..."
+        />
+      </section>
+      <section className={style.Butttons}>
+        <button className={`${style.ButtonArrow} ${style.Button}`}>
+          <IconArrow rotate="0deg" />
+        </button>
+        <button className={`${style.ButtonArrow} ${style.Button}`}>
+          <IconArrow />
+        </button>
+        <button className={`${style.ButtonFilter} ${style.Button}`}>
+          <IconFilter />
+          Filtros
+        </button>
+      </section>
+    </section>
+  );
+};
+
+export default FilterSection;
