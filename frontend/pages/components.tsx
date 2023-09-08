36fa1f467dc0464cae7de63b0ea6570db5991fdd
Jose Waldo <josferw@gmail.com>
2023-09-08 08:48:01 -0500

    Probe el componente filter-section y info-user

--- frontend/pages/components.tsx
+++ frontend/pages/components.tsx
@@ -1,27 +1,31 @@
+import FilterSection from "@/components/filter-section/FilterSection";
+import InfoUser from "@/components/info-user/InfoUser";
 import Table from "@/components/table/Table";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 function Components() {
-  const [isLogin, setIsLogin] = useState<boolean>(false);
+  const [isLogin, setIsLogin] = useState<boolean>(true);
   const router = useRouter();
 
-  useEffect(() => {
-    const auth = sessionStorage.getItem("authInfo");
-    if (auth) {
-      setIsLogin(true);
-      return;
-    } else {
-      router.push("/newlogin");
-      return;
-    }
-  }, []);
+  // useEffect(() => {
+  //   const auth = sessionStorage.getItem("authInfo");
+  //   if (auth) {
+  //     setIsLogin(true);
+  //     return;
+  //   } else {
+  //     router.push("/newlogin");
+  //     return;
+  //   }
+  // }, []);
 
   if (isLogin) {
     return (
       <main className="p-10">
         <h1 className="mb-[2rem]">Vista de los componentes</h1>
         <Table />
+        <InfoUser name="Pedro Rodriguez" role="Administrador" />
+        <FilterSection />
       </main>
     );
   }
