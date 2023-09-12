6b6ef1f68b1ce7c42217e9c7839a5e723f8487a4
Jose Waldo <josferw@gmail.com>
2023-09-11 21:51:32 -0500

    feat: cree una función para validar los campos del formulario

--- frontend/functions/validations.ts
+++ frontend/functions/validations.ts
@@ -1,4 +1,5 @@
 import { Credentials } from "@/types/types";
+import { UserForm } from "@/components/form-add-user/form-add-user.types";
 
 interface ResponseValidateCredentials {
   isValidate: boolean;
@@ -22,4 +23,31 @@ export const validateCredentials = ({
   }
 
   return { isValidate: false, message };
+};
+
+export const validateFieldsAddForm = (
+  userFormData: UserForm
+): ResponseValidateCredentials => {
+  let message = "";
+  let isValidate = false;
+
+  const {
+    name,
+    lastName,
+    username,
+    document,
+    position,
+    email,
+    university,
+    location,
+  } = userFormData;
+
+  const fieldEmpty = Object.values(userFormData).filter(
+    (elem) => elem.length === 0
+  );
+
+  if (fieldEmpty.length === 0)
+    return { isValidate, message: "Error, todos los campos son obligatorios" };
+
+  return { isValidate, message };
 };
