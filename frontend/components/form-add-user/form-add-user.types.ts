691d6bf9584911600c088ae3edc9d608e387b582
Jose Waldo <josferw@gmail.com>
2023-09-11 21:51:02 -0500

    feat: añadi tipos para el formulario de un nuevo usuario

---
+++ frontend/components/form-add-user/form-add-user.types.ts
@@ -0,0 +1,10 @@
+export interface UserForm {
+  name: string;
+  lastName: string;
+  username: string;
+  email: string;
+  document: string;
+  position: string;
+  location: string;
+  university: string;
+}
