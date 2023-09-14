2fb9e8bb67a26a2cffdd0255ce064e8bfbce65c3
Jose Waldo <josferw@gmail.com>
2023-09-14 15:53:23 -0500

    feat: cree la estructura básica el formulario

---
+++ frontend/components/form-update-user/FormUpdateUser.tsx
@@ -0,0 +1,11 @@
+import styles from "./form-update-user.module.css";
+
+const FormUpdateUser = () => {
+  return (
+    <form method="POST">
+      <h2>Actualizar Información</h2>
+    </form>
+  );
+};
+
+export default FormUpdateUser;
