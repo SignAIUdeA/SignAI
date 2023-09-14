9ae2ceb5c8c48ec4ddcb32a76294b7a2cecaec5a
Jose Waldo <josferw@gmail.com>
2023-09-14 18:04:15 -0500

    cambio de nombre

--- frontend/components/form-update-user/form-update-user.types.ts
+++
@@ -1,7 +0,0 @@
-import { StringDictionary } from "@/hooks/useForm";
-
-export interface UpdateCredentialsUser extends StringDictionary {
-  user: string;
-  password: string;
-  confirmPassword: string;
-}
