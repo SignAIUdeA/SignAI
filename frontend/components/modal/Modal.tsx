b7cd75eaacc4147d2c45c14aaa285806bece2dc1
Jose Waldo <josferw@gmail.com>
2023-09-14 17:49:25 -0500

    feat: añadi el boton de cerrar y funcionalidad para visualizar el modal

--- frontend/components/modal/Modal.tsx
+++ frontend/components/modal/Modal.tsx
@@ -1,14 +1,26 @@
+import { IconClose } from "@/icons/Icons";
 import styles from "./modal.module.css";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 
 interface Props {
   children?: ReactNode;
+  closeButton?: boolean;
+  setShowModal: (showModal: boolean) => void;
 }
 
-const Modal = ({ children }: Props) => {
+const Modal = ({ children, closeButton = true, setShowModal }: Props) => {
   return (
     <main className={styles.ModalContainer}>
-      <section className={styles.Modal}>{children}</section>
+      <section className={styles.Modal}>
+        {!closeButton || (
+          <button
+            className={styles.BtnClose}
+            onClick={() => setShowModal(false)}>
+            <IconClose width="1rem" height="1rem" />
+          </button>
+        )}
+        {children}
+      </section>
     </main>
   );
 };
