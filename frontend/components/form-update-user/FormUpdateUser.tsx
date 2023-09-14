b6865a92285adb641e98ab6e89a8790564156f50
Jose Waldo <josferw@gmail.com>
2023-09-14 18:02:22 -0500

    feat: añadi el useForm para leer los valores de los inputs

--- frontend/components/form-update-user/FormUpdateUser.tsx
+++ frontend/components/form-update-user/FormUpdateUser.tsx
@@ -1,20 +1,39 @@
 import styles from "./form-update-user.module.css";
+import useForm from "@/hooks/useForm";
+import { UpdateCredentialsUser } from "./form-update-user.types";
 
 interface Props {
   setShowModal: (showModal: boolean) => void;
 }
 
+const userUpdateCredentialsDataForm: UpdateCredentialsUser = {
+  user: "",
+  password: "",
+  confirmPassword: "",
+};
+
 const FormUpdateUser = ({ setShowModal }: Props) => {
+  const { inputs, handleChange } = useForm<UpdateCredentialsUser>(
+    userUpdateCredentialsDataForm
+  );
+
   return (
     <form method="POST" className={styles.Form}>
       <h2 className={styles.FormTitle}>Actualizar Información</h2>
-      <label htmlFor="name">
-        Nombres
-        <input className={styles.Input} type="text" name="name" id="name" />
+      <label htmlFor="user">
+        Nuevo Usuario
+        <input
+          onChange={handleChange}
+          className={styles.Input}
+          type="text"
+          name="user"
+          id="user"
+        />
       </label>
       <label htmlFor="password">
         Contraseña
         <input
+          onChange={handleChange}
           className={styles.Input}
           type="password"
           name="password"
@@ -24,6 +43,7 @@ const FormUpdateUser = ({ setShowModal }: Props) => {
       <label htmlFor="confirmPassword">
         Repetir contraseña
         <input
+          onChange={handleChange}
           className={styles.Input}
           type="password"
           name="confirmPassword"
