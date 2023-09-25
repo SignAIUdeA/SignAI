c262c551945cd272cca03830cc9f5ba30e903eb2
Jose Waldo <josferw@gmail.com>
2023-09-25 08:08:57 -0500

    se añadio al hook la información del usuario

--- frontend/hooks/useLogin.ts
+++ frontend/hooks/useLogin.ts
@@ -1,12 +1,14 @@
-import { AuthResponse, UserAuthorized } from "@/types/types";
+import { AuthResponse, UserAuthorized, UserInfo } from "@/types/types";
 import { useEffect, useState } from "react";
 import jwt_decode from "jwt-decode";
 import { useRouter } from "next/router";
+import { getUserInfo } from "@/functions/login";
 
 const useLogin = () => {
   const router = useRouter();
   const [isLogin, setIsLogin] = useState<boolean>(false);
   const [userAuth, setUserAuth] = useState<UserAuthorized>();
+  const [userInfo, setUserInfo] = useState<UserInfo>();
 
   useEffect(() => {
     const authInfo = sessionStorage.getItem("authInfo");
@@ -17,6 +19,7 @@ const useLogin = () => {
       ) as UserAuthorized;
       setUserAuth(infoToken);
       setIsLogin(true);
+      getUserInfo(authInfoParse).then((res) => setUserInfo(res as UserInfo));
     } else {
       router.push("/login");
     }
@@ -25,6 +28,7 @@ const useLogin = () => {
   return {
     isLogin,
     userAuth,
+    userInfo,
   };
 };
 
