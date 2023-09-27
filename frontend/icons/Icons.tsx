6a04cb234eabdcb575263f87efefb735fa3ffb63
Jose Waldo <josferw@gmail.com>
2023-09-27 10:31:36 -0500

    Feat: creación del icono de menu hamburguesa y corrección de los estilos del boton

--- frontend/icons/Icons.tsx
+++ frontend/icons/Icons.tsx
@@ -265,3 +265,20 @@ export const IconCheckSuccesfully = ({
     </svg>
   );
 };
+
+export const IconMenuHamburger = ({
+  width = DEFAULT_SIZE_ICONS,
+  height = DEFAULT_SIZE_ICONS,
+  fill = WHITE,
+}: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      fill={fill}
+      viewBox="0 0 24 24">
+      <path d="M4 11h12v2H4zm0-5h16v2H4zm0 12h7.235v-2H4z" />
+    </svg>
+  );
+};
