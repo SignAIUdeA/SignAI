2d66f52e4fc7419b74b29cc02e379d2e5e390758
Jose Waldo <josferw@gmail.com>
2023-10-03 07:30:27 -0500

    Ahora se está consumiendo la información del usuario desde el back

--- frontend/components/config-profile/ConfigProfile.tsx
+++ frontend/components/config-profile/ConfigProfile.tsx
@@ -14,18 +14,21 @@ import {
 import TagPosition from "../tag-position/TagPosition";
 import Modal from "../modal/Modal";
 import FormUpdateCredentialUser from "../form-update-credential-user/FormUpdateCredentialUser";
+import useLogin from "@/hooks/useLogin";
 
 const ConfigProfile = () => {
+  const { userInfo } = useLogin();
+
   const [showModal, setShowModal] = useState<boolean>(false);
   const profileData: ProfileData = {
-    name: "MANUEL TOBIAS",
+    name: userInfo?.name || "MANUEL TOBIAS",
     lastName: "GARCIA CUELLO",
-    location: "Río Negro, Antioquia",
-    university: "Universidad de Antioquia",
+    location: userInfo?.location || "Río Negro, Antioquia",
+    university: userInfo?.university || "Universidad de Antioquia",
     date: "Ago 22 - 2023",
-    username: "manuel.garcia",
-    email: "manuel.garcia@udea.edu.co",
-    document: "1000907416",
+    username: userInfo?.name || "manuel.garcia",
+    email: userInfo?.email || "manuel.garcia@udea.edu.co",
+    document: userInfo?.documentId || "1000907416",
     password: "contra123",
     position: "Profesional LSC",
   };
@@ -39,61 +42,6 @@ const ConfigProfile = () => {
     <MdRemoveRedEye key="password" className="h-8 w-8" />,
   ];
   return (
-    /*
-        
-        <div className="flex flex-col items-center mb-4 debug">
-      <IconAvatarUser width="100px" height="100px" />
-      <section className={`${styles.SectionData} `}>
-        <div>
-          <h3 className={`${styles.FormTitle} `}>{profileData.name}</h3>
-          <h3 className={`${styles.FormTitle} `}>{profileData.lastName}</h3>
-        </div>
-        <ul className=" flex flex-col gap-4">
-          <li className="flex gap-4">
-            <MdLocationOn className="h-8 w-8" />
-            <span>{profileData.location}</span>
-          </li>
-          <li className="flex gap-4">
-            <MdLocationCity className="h-8 w-8" />
-            <span>{profileData.university}</span>
-          </li>
-          <li className="flex gap-4">
-            <MdDateRange className="h-8 w-8" />
-            <span>{profileData.date}</span>
-          </li>
-          <li className="flex gap-4">
-            <MdPerson className="h-8 w-8" />
-            <span>{profileData.username}</span>
-          </li>
-          <li className="flex gap-4">
-            <MdMailOutline className="h-8 w-8" />
-            <span>{profileData.email}</span>
-          </li>
-          <li className="flex gap-4">
-            <MdOutlinePermContactCalendar className="h-8 w-8" />
-            <span>{profileData.document}</span>
-          </li>
-          <li className="flex gap-4">
-            <MdRemoveRedEye className="h-8 w-8" />
-            <span>{profileData.password}</span>
-          </li>
-        </ul>
-        <TagPosition />
-        <button
-          className={styles.BtnForm}
-          onClick={() => setShowModal(!showModal)}>
-          Actualizar Información
-        </button>
-        {!showModal || (
-          <Modal setShowModal={setShowModal} closeButton={false}>
-            <FormUpdateCredentialUser setShowModal={setShowModal} />
-          </Modal>
-        )}
-      </section>
-    </div>
-        
-        */
-
     <section className={styles.ProfileSection}>
       <div className={styles.WrapperAvatar}>
         <IconAvatarUser width="80px" height="80px" />
