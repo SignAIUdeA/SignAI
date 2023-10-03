9ab61986b7dca3ccabbae4117ef182be3c8a2f05
Jose Waldo <josferw@gmail.com>
2023-10-03 07:25:51 -0500

    Removi algunos componentes ya que se estan usando en el lugar adecuado

--- frontend/pages/components.tsx
+++ frontend/pages/components.tsx
@@ -1,48 +1,24 @@
 import { ConfigProfile } from "@/components/config-profile/ConfigProfile";
-import FilterSection from "@/components/filter-section/FilterSection";
-import FormAddUser from "@/components/form-add-user/FormAddUser";
 import FormUpdateCredentialUser from "@/components/form-update-credential-user/FormUpdateCredentialUser";
-import InfoUser from "@/components/info-user/InfoUser";
 import Modal from "@/components/modal/Modal";
-import Table from "@/components/table/Table";
-import useLogin from "@/hooks/useLogin";
-import { RoleType } from "@/types/types";
 import { useState } from "react";
 import TagPosition from "@/components/tag-position/TagPosition";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-
-const ROLES = {
-  administrator: "Administrador",
-  assistant: "Auxiliar",
-  professional: "Profesional",
-};
 
 function Components() {
   const [showModal, setShowModal] = useState<boolean>(false);
-  const { isLogin, userAuth, userInfo } = useLogin();
 
-  if (isLogin) {
-    const role = userAuth?.role as RoleType;
-
-    return (
-      <main className="p-10">
-        <h1 className="mb-[2rem]">Vista de los componentes</h1>
-        <Table />
-        <InfoUser name={userInfo?.name as string} role={ROLES[role]} />
-        <FilterSection />
-        <FormAddUser />
-        <button onClick={() => setShowModal(!showModal)}>Abrir Modal</button>
-        {!showModal || (
-          <Modal setShowModal={setShowModal} closeButton={false}>
-            <FormUpdateCredentialUser setShowModal={setShowModal} />
-          </Modal>
-        )}
-        <ConfigProfile />
-        <TagPosition />
-      </main>
-    );
-  }
+  return (
+    <main className="p-10">
+      <button onClick={() => setShowModal(!showModal)}>Abrir Modal</button>
+      {!showModal || (
+        <Modal setShowModal={setShowModal} closeButton={false}>
+          <FormUpdateCredentialUser setShowModal={setShowModal} />
+        </Modal>
+      )}
+      <ConfigProfile />
+      <TagPosition />
+    </main>
+  );
 }
 
 export default Components;
