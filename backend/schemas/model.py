d124fe091f346d279e1c4c6a9093595fae00f000
Jose Waldo <josferw@gmail.com>
2023-10-18 09:46:26 -0500

    Feat: añadi el esquema del Modelo

---
+++ backend/schemas/model.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from typing import List
+from datetime import datetime
+
+
+class ModelBase(BaseModel):
+    name: str
+    description: str
+    category: str
+    key_words: List[str]
+    precision: str
+    sensitivity: str
+    specificity: str
+    f1_score: str
+    roc_auc: str
+    version: str
+    notes_version: str
+    state_investigation: str
+    comments: str
+    created_by: str
+    creation_date: str
