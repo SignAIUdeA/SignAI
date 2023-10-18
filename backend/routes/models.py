f21da78f06c7145ee4413baf3a03c79d433c70d8
Jose Waldo <josferw@gmail.com>
2023-10-18 11:15:29 -0500

    Feat: añadi paginación a la lista de modelos

--- backend/routes/models.py
+++ backend/routes/models.py
@@ -3,6 +3,10 @@ from security import oauth2
 from schemas.model import *
 from functions import models
 
+from fastapi_pagination import add_pagination, paginate
+from fastapi_pagination.links import Page
+
+
 router = APIRouter(prefix="/model", tags=["Models"])
 
 
@@ -11,6 +15,10 @@ def create_model(request: ModelBase):
     return models.create(request)
 
 
-@router.get("/", response_model=list[ModelBase], status_code=200)
+@router.get("/", response_model=Page[ModelBase], status_code=200)
 def find_all_models():
-    return models.find_all()
+    list_models = models.find_all()
+    return paginate(list_models)
+
+
+add_pagination(router)
