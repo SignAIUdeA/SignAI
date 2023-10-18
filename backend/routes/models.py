e6e21f91c9e067c1a5c720e82c2d6671910dedea
Jose Waldo <josferw@gmail.com>
2023-10-18 10:48:17 -0500

    Feat: añadi la ruta para obtener todos los modelos

--- backend/routes/models.py
+++ backend/routes/models.py
@@ -9,3 +9,8 @@ router = APIRouter(prefix="/model", tags=["Models"])
 @router.post("/", response_model=ModelBase, status_code=201)
 def create_model(request: ModelBase):
     return models.create(request)
+
+
+@router.get("/", response_model=list[ModelBase], status_code=200)
+def find_all_models():
+    return models.find_all()
