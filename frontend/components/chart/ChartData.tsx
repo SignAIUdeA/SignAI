07ea1bcbb44e7f4de7bae77f5d3380960fe50673
Jose Waldo <josferw@gmail.com>
2023-10-31 09:02:41 -0500

    Style: añadi más estilos al componente de la gráfica de análisis

--- frontend/components/chart/ChartData.tsx
+++ frontend/components/chart/ChartData.tsx
@@ -1,56 +1,63 @@
-import React from 'react'
-import {Bar} from 'react-chartjs-2';
-import {Chart as ChartJS, LinearScale} from 'chart.js/auto';
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import { Chart as ChartJS, LinearScale } from "chart.js/auto";
+import { Jost, Poppins } from "next/font/google";
+
+const tipografia = Jost({ subsets: ["latin"], weight: ["500", "600", "700"] });
 
 ChartJS.register(LinearScale);
 
 const ChartData = ({ charData }) => {
-    return (
-      <Bar
-        width={600}
-        height={300}
-        data={charData}
-        options={{
-          indexAxis: 'y',
-          responsive: true,
-          scales: {
-            x: {
-              title: {
-                display: true,
-                text: 'Cantidad de datos', // Título del eje X
-                font: {
-                  size: 14, // Tamaño de fuente del título
-                  weight: 'bold', // Peso de la fuente del título
-                },
-                color: '#026937',
+  const fontFamily = tipografia.style.fontFamily;
+
+  return (
+    <Bar
+      width={600}
+      height={300}
+      data={charData}
+      options={{
+        indexAxis: "y",
+        responsive: true,
+        scales: {
+          x: {
+            title: {
+              display: true,
+              text: "Cantidad de datos", // Título del eje X
+              font: {
+                size: 16, // Tamaño de fuente del título
+                weight: "bold",
+                family: fontFamily, // Peso de la fuente del título
               },
-              ticks: {
-                padding: 10, // Ajuste de padding para las etiquetas en el eje X
-                color: 'dark'
-              },
+              color: "#026937",
             },
-            y: {
-              title: {
-                display: true,
-                text: 'Etiquetas', // Título del eje Y
-                font: {
-                  size: 14, // Tamaño de fuente del título
-                  weight: 'bold', // Peso de la fuente del título
-                },
-                color: '#026937',
-              },
-              ticks: {
-                padding: 10, // Ajuste de padding para las etiquetas en el eje X
-                color: 'dark',
-                
-              },
+            ticks: {
+              padding: 10, // Ajuste de padding para las etiquetas en el eje X
+              color: "dark",
             },
           },
-        }}
-        className='debug'
-      />
-    );
-  };
+          y: {
+            title: {
+              display: true,
+              text: "Etiquetas", // Título del eje Y
+              font: {
+                size: 16, // Tamaño de fuente del título
+                weight: "bold",
+                family: fontFamily, // Peso de la fuente del título
+              },
+              color: "#026937",
+            },
+            grid: {
+              display: false,
+            },
+            ticks: {
+              padding: 10, // Ajuste de padding para las etiquetas en el eje X
+              color: "dark",
+            },
+          },
+        },
+      }}
+    />
+  );
+};
 
-  export default ChartData
-
+export default ChartData;
