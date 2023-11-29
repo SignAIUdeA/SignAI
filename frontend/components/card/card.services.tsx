8171fced0b370e8c7b58236b46b4ff89f298a4b6
Jose Waldo <josferw@gmail.com>
2023-11-29 13:44:57 -0500

    Se expuso la interfaz de CardSignType

--- frontend/components/card/card.services.tsx
+++ frontend/components/card/card.services.tsx
@@ -1,65 +1,63 @@
-import React from 'react'
-import { MdDateRange, MdLabelOutline, MdOutlinePermIdentity } from 'react-icons/md'
-interface CardType {
-    name: string,
-    date: string,
-    label: string,
-    image: string,
-} 
+export interface CardSignType {
+  name: string;
+  date: string;
+  label: string;
+  image: string;
+}
 
-export const CardData: CardType[] = [
-    {
-        name: 'Santiago Bedoya',
-        date: '6/09/2023',
-        label: 'hola',
-        image: '/sign.png',
-    },
-    {
-        name: 'Andrés Quintero',
-        date: '6/09/2023',
-        label: 'hola',
-        image: '/sign.png',
-    },
-    {
-        name: 'Jose Waldo',
-        date: '6/09/2023',
-        label: 'hola',
-        image: '/sign.png',
-    },
-    {
-        name: 'Santiago Bedoya',
-        date: '6/09/2023',
-        label: 'hola',
-        image: '/sign.png',
-    },
-    {
-        name: 'Andrés Quintero',
-        date: '6/09/2023',
-        label: 'hola',
-        image: '/sign.png',
-    },
-    {
-        name: 'Andrés Quintero',
-        date: '6/09/2023',
-        label: 'hola',
-        image: '/sign.png',
-    },
-    {
-        name: 'Andrés Quintero',
-        date: '6/09/2023',
-        label: 'hola',
-        image: '/sign.png',
-    },
-    {
-        name: 'Andrés Quintero',
-        date: '6/09/2023',
-        label: 'hola',
-        image: '/sign.png',
-    },
-    {
-        name: 'Jose Waldo',
-        date: '6/09/2023',
-        label: 'hola',
-        image: '/sign.png',
-    }
-]
+export const CardData: CardSignType[] = [
+  {
+    name: "Santiago Bedoya",
+    date: "6/09/2023",
+    label: "hola",
+    image: "/sign.png",
+  },
+  {
+    name: "Andrés Quintero",
+    date: "6/09/2023",
+    label: "hola",
+    image: "/sign.png",
+  },
+  {
+    name: "Jose Waldo",
+    date: "6/09/2023",
+    label: "hola",
+    image: "/sign.png",
+  },
+  {
+    name: "Santiago Bedoya",
+    date: "6/09/2023",
+    label: "hola",
+    image: "/sign.png",
+  },
+  {
+    name: "Andrés Quintero",
+    date: "6/09/2023",
+    label: "hola",
+    image: "/sign.png",
+  },
+  {
+    name: "Andrés Quintero",
+    date: "6/09/2023",
+    label: "hola",
+    image: "/sign.png",
+  },
+  {
+    name: "Andrés Quintero",
+    date: "6/09/2023",
+    label: "hola",
+    image: "/sign.png",
+  },
+  {
+    name: "Andrés Quintero",
+    date: "6/09/2023",
+    label: "hola",
+    image: "/sign.png",
+  },
+  {
+    name: "Jose Waldo",
+    date: "6/09/2023",
+    label: "hola",
+    image: "/sign.png",
+  },
+];
