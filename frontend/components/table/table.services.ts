22bed1564981977cd93d5ae26bd403374e1246e4
Jose Waldo <josferw@gmail.com>
2023-11-29 13:42:19 -0500

    Feat: añadi la funcion para formatear las fechas

--- frontend/components/table/table.services.ts
+++ frontend/components/table/table.services.ts
@@ -4,18 +4,30 @@ const users: User[] = [
   {
     name: "Hernando Cuesta",
     position: "Profesional",
-    created_at: "Ago 23 - 2023"
+    created_at: "Ago 23 - 2023",
   },
   {
     name: "Maria Castellanos",
     position: "Profesional",
-    created_at: "Ago 23 - 2023"
+    created_at: "Ago 23 - 2023",
   },
   {
     name: "Alejandra Perez",
     position: "Auxiliar",
-    created_at: "Ago 23 - 2023"
-  }
-]
+    created_at: "Ago 23 - 2023",
+  },
+];
 
-export const getAllUsers = (): User[] => users
+export const formatDate = (fechaOriginal: string): string => {
+  const fecha = new Date(fechaOriginal);
+
+  const formatoFecha = fecha.toLocaleDateString("es-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
+  return formatoFecha;
+};
+
+export const getAllUsers = (): User[] => users;
