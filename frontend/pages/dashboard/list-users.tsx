514c2539fd14ead70269c3639f4a5784030b6acd
Jose Waldo <josferw@gmail.com>
2023-11-29 13:18:00 -0500

    Feat: se añadió la página para ver la lista de usuarios con la paginación

--- frontend/pages/dashboard/list-users.tsx
+++ frontend/pages/dashboard/list-users.tsx
@@ -1,13 +1,33 @@
 import FilterSection from "@/components/filter-section/FilterSection";
 import Table from "@/components/table/Table";
+import { getUsers } from "@/functions/users";
 import Layout from "@/layouts/Layout";
+import { UserTableInfo } from "@/types/types";
+import { use, useEffect, useState } from "react";
 
 const ListUsers = () => {
+  const [listUsers, setListUsers] = useState<UserTableInfo[]>([]);
+  const [page, setPage] = useState<number>(1);
+  const [maxPage, setMaxPage] = useState<number>(1);
+
+  useEffect(() => {
+    getUsers(page).then((res) => {
+      const { data } = res;
+      const { items: list } = data;
+      setMaxPage(data.pages);
+      setListUsers(list);
+    });
+  }, [page]);
+
   return (
     <Layout>
       <div className="flex flex-col w-[100%] h-[100%] p-[2rem]">
-        <FilterSection />
-        <Table />
+        <FilterSection page={page} setPage={setPage} maxPages={maxPage} />
+        {listUsers.length === 0 ? (
+          "No hay usuarios"
+        ) : (
+          <Table listUsers={listUsers} />
+        )}
       </div>
     </Layout>
   );
