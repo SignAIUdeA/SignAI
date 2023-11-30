23f380e51156150d688d5696505947cff9fbe69f
Jose Waldo <josferw@gmail.com>
2023-11-30 08:23:36 -0500

    Feat: se añadieron endpoints para hacer busquedas y eliminar por id

--- backend/routes/user.py
+++ backend/routes/user.py
@@ -3,6 +3,9 @@ from security import oauth2
 from schemas.user import *
 from functions import user
 
+from fastapi_pagination import add_pagination, paginate
+from fastapi_pagination.links import Page
+
 router = APIRouter(prefix="/user", tags=["Users"])
 
 
@@ -16,9 +19,15 @@ def get_user(current_user: CurrentUser = Depends(oauth2.get_current_user)):
     return user.show(current_user)
 
 
-@router.get("/all", response_model=List[UserAllInfomation], status_code=200)
-def get_user():
-    return user.show_all_users()
+@router.get("/all", response_model=Page[UserAllInfomation], status_code=200)
+def get_all_users():
+    list_users = user.show_all_users()
+    return paginate(list_users)
+
+
+@router.get("/{id}", response_model=UserAllInfomation, status_code=200)
+def get_user_by_id(id):
+    return user.found_user(id)
 
 
 @router.put("/", response_model=ShowUser, status_code=200)
@@ -26,6 +35,19 @@ def update_user(request: EditUser, current_user: CurrentUser = Depends(oauth2.ge
     return user.update(current_user, request)
 
 
+@router.put("/update-credentials", status_code=204)
+def update_user_credentials(request: EditCredentials, current_user: CurrentUser = Depends(oauth2.get_current_user)):
+    return user.update_credentials(current_user, request)
+
+
 @router.delete("/", status_code=204)
 def delete_user(current_user: CurrentUser = Depends(oauth2.get_current_user)):
     return user.delete(current_user)
+
+
+@router.delete("/{id}", status_code=204)
+def delete_user_by_id(id):
+    return user.delete_user(id)
+
+
+add_pagination(router)
