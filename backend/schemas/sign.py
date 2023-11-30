cb6107872119e35f07e871cd71d294dd03e40824
Jose Waldo <josferw@gmail.com>
2023-11-30 11:43:45 -0500

    Feat: se añadió y cambio los esquemas para las señas

--- backend/schemas/sign.py
+++ backend/schemas/sign.py
@@ -2,13 +2,21 @@ from typing import List, Optional
 from pydantic import BaseModel
 from datetime import datetime
 
+
 class SignBase(BaseModel):
+    path_file: str
     label: str
-    fecha_creacion: datetime
-    fecha_modificacion: datetime
+    creation_date: datetime
+    role_user: str
+    upload_by: str
 
     class Config:
         orm_mode = True
+
+
+class CreateSign(BaseModel):
+    label: str
+
 
 class ShowSign(BaseModel):
     label: str
