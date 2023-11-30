73e5a2e06b46958e22a35c031597d86796293eec
Jose Waldo <josferw@gmail.com>
2023-11-30 11:44:15 -0500

    Refactor: se cambió el endpoint de subir una seña

--- backend/routes/sign.py
+++ backend/routes/sign.py
@@ -1,5 +1,6 @@
+import json
 from typing import List
-from fastapi import APIRouter, Depends, status, File, UploadFile
+from fastapi import APIRouter, Depends, status, File, UploadFile, Form
 from schemas.sign import *
 from schemas.user import CurrentUser
 from functions import sign
@@ -21,16 +22,23 @@ def get_all_signs(current_user: CurrentUser = Depends(oauth2.get_current_user)):
     return sign.get_all(current_user.email)
 
 
-@router.post("/", status_code=status.HTTP_201_CREATED, response_model=ShowSign)
-def create_sign_and_upload_file(
-    request: SignBase,
-    file: UploadFile = File(...),
-    current_user: CurrentUser = Depends(oauth2.get_current_user),
-):
-    created_sign = sign.create(request, current_user)
-    sign_id = created_sign.id
-    sign.upload_file(sign_id, file, current_user)
-    return sign.show(sign_id)       
+# @router.post("/", status_code=status.HTTP_201_CREATED, response_model=ShowSign)
+# def create_sign_and_upload_file(
+#     request: SignBase,
+#     file: UploadFile = File(...),
+#     current_user: CurrentUser = Depends(oauth2.get_current_user),
+# ):
+#     created_sign = sign.create(request, current_user)
+#     sign_id = created_sign.id
+#     sign.upload_file(sign_id, file, current_user)
+#     return sign.show(sign_id)
+@router.post("/", status_code=status.HTTP_201_CREATED)
+def create_sign_and_upload_file(metadata: str = Form(...), file: UploadFile = File(...), current_user: CurrentUser = Depends(oauth2.get_current_user)):
+    metadata = json.loads(metadata)
+    create_sign: CreateSign = {
+        "label": metadata["label"],
+    }
+    return sign.create(create_sign, current_user, file)
 
 
 @router.put("/{id}", response_model=ShowSign, status_code=status.HTTP_200_OK)
