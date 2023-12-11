997e8470dd935a48394b4b70e26ce60118b40088
Jose Waldo <josferw@gmail.com>
2023-12-11 17:43:29 -0500

    Feat: llamado al back para traer la información

--- frontend/pages/dashboard/data-analysis.tsx
+++ frontend/pages/dashboard/data-analysis.tsx
@@ -1,10 +1,13 @@
 import Layoutp from "@/layouts/Layout";
 import { labelsData } from "@/components/chart/chart.services";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ChartData from "@/components/chart/ChartData";
 import styles from "@/styles/data-analysis.module.css";
+import { getInfoSigs } from "@/functions/sign";
+import { DataAnalysisInfo } from "@/types/types";
 
 const DataAnalysis = () => {
+  const [numberVideos, setNumberVideos] = useState(0);
   const [userLabelData, setUserLabelData] = useState({
     labels: labelsData.map((data) => data.sign_desc),
     datasets: [
@@ -20,13 +23,38 @@ const DataAnalysis = () => {
     ],
   });
 
+  useEffect(() => {
+    getInfoSigs()
+      .then((respose) => {
+        const data: DataAnalysisInfo = respose.data;
+        const labels = Object.keys(data.labels_info);
+        const numberLabel = Object.values(data.labels_info);
+        setNumberVideos(data["number_signs"]);
+        setUserLabelData({
+          labels,
+          datasets: [
+            {
+              axis: "y",
+              label: "Count",
+              data: numberLabel,
+              backgroundColor: ["green"],
+              borderRadius: 5,
+              barPercentage: 0.2,
+              borderSkipped: false,
+            },
+          ],
+        });
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <Layoutp>
       <section className={styles.Wrapper}>
         <header className={styles.Header}>
           <h3 className={styles.Title}>Análisis de Datos</h3>
           <div className={styles.Information}>
-            <span className={styles.Cantidad}>375</span>
+            <span className={styles.Cantidad}>{numberVideos}</span>
             <span className={styles.Message}>Videos e Imagenes</span>
           </div>
         </header>
