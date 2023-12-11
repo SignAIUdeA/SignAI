647669d5220aa62d4b9a7c22ab255b9f6a3775fd
Jose Waldo <josferw@gmail.com>
2023-12-11 17:38:05 -0500

    Feat: añadi el esquema de ShowModel

--- backend/schemas/model.py
+++ backend/schemas/model.py
@@ -19,3 +19,22 @@ class ModelBase(BaseModel):
     comments: str
     created_by: str
     creation_date: str
+
+
+class ShowModel(BaseModel):
+    id: str
+    name: str
+    description: str
+    category: str
+    key_words: List[str]
+    precision: str
+    sensitivity: str
+    specificity: str
+    f1_score: str
+    roc_auc: str
+    version: str
+    notes_version: str
+    state_investigation: str
+    comments: str
+    created_by: str
+    creation_date: str
