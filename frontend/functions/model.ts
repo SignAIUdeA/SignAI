adfeabc130fe00d456c4056302f8a3e96e19ea5b
Jose Waldo <josferw@gmail.com>
2023-12-11 17:46:24 -0500

    Feat: añadi el  metodo para obtener un archivo y modifique el envio de datos

--- frontend/functions/model.ts
+++ frontend/functions/model.ts
@@ -1,12 +1,9 @@
-import { Modelo } from "@/components/card-model/mock";
-import { NewModelInteface } from "@/types/types";
 import axios from "axios";
 
 interface RequestResponse {
   ok: boolean;
   message: string;
   data?: any;
-  //authResponse?: AuthResponse;
 }
 
 const instance = axios.create({
@@ -16,12 +13,30 @@ const instance = axios.create({
 });
 
 export const createNewModel = async (
-  newModel: NewModelInteface
+  newModel: string,
+  file: File
 ): Promise<RequestResponse> => {
+  const formData = new FormData();
+
+  formData.append("file", file);
+  formData.append("metadata", newModel);
   try {
-    const response = await instance.post("/model", newModel);
-    const data = response.data;
-    return { ok: true, message: "¡El modelo se ha creado exitosamente!" };
+    const config = {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    };
+
+    const response = await axios.post(
+      "http://127.0.0.1:8000/model/",
+      formData,
+      config
+    );
+
+    return {
+      ok: true,
+      message: "Se ha creado la seña correctamente",
+    };
   } catch (error: any) {
     return { ok: false, message: error.response.data.detail };
   }
@@ -29,12 +44,22 @@ export const createNewModel = async (
 
 export const getModels = async (
   page: number,
-  size = 3
+  size = 6
 ): Promise<RequestResponse> => {
   try {
     const response = await instance.get(`/model?page=${page}&size=${size}`);
     const data = response.data;
     return { ok: true, message: "¡Modelos obtenidos exitosamente!", data };
+  } catch (error: any) {
+    return { ok: false, message: error.response.data.detail };
+  }
+};
+
+export const getModelFile = async (id: number): Promise<RequestResponse> => {
+  try {
+    const response = await instance.get(`/model/file/${id}`);
+    const data = response.data;
+    return { ok: true, message: "¡Modelo obtenido exitosamente!", data };
   } catch (error: any) {
     return { ok: false, message: error.response.data.detail };
   }
