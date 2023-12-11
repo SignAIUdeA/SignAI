ceef02f6fbe963badce6c4793ab0e1cbc1731bdc
Jose Waldo <josferw@gmail.com>
2023-12-11 17:45:05 -0500

    Refactor: modifique la lista para omitir a quien inicia sesión

--- frontend/pages/dashboard/list-users.tsx
+++ frontend/pages/dashboard/list-users.tsx
@@ -2,6 +2,7 @@ import FilterSection from "@/components/filter-section/FilterSection";
 import Table from "@/components/table/Table";
 import { getUsers } from "@/functions/users";
 import Layout from "@/layouts/Layout";
+import { useUserStore } from "@/store/userStore";
 import { UserTableInfo } from "@/types/types";
 import { use, useEffect, useState } from "react";
 
@@ -9,13 +10,15 @@ const ListUsers = () => {
   const [listUsers, setListUsers] = useState<UserTableInfo[]>([]);
   const [page, setPage] = useState<number>(1);
   const [maxPage, setMaxPage] = useState<number>(1);
+  const userLogin = useUserStore((state) => state.userInfo);
 
   useEffect(() => {
     getUsers(page).then((res) => {
       const { data } = res;
-      const { items: list } = data;
+      const list: UserTableInfo[] = data.items;
       setMaxPage(data.pages);
-      setListUsers(list);
+      const newList = list.filter((user) => user.email !== userLogin?.email);
+      setListUsers(newList);
     });
   }, [page]);
 
@@ -24,7 +27,7 @@ const ListUsers = () => {
       <div className="flex flex-col w-[100%] h-[100%] p-[2rem]">
         <FilterSection page={page} setPage={setPage} maxPages={maxPage} />
         {listUsers.length === 0 ? (
-          "No hay usuarios"
+          <h3 className="text-gray-800">No hay usuarios...</h3>
         ) : (
           <Table listUsers={listUsers} />
         )}
